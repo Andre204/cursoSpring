6faea47a510a02354dfeb2c07ebb8bfc9ce484a5
André Luiz <aluiz204@gmail.com>
2021-02-04 08:46:32 -0300

    Projeto criado

---
+++ src/test/java/curso/andre/cursoSpring/CursoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.andre.cursoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
